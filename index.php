529447803b8dadb323f870d2812a93eece92f324
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-04-08 15:39:45 +0200

    snack 2

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 </head>
 
 <body>
-    <!-- SNACK 1 -->
+    <h1>PHP Snacks blocco 1</h1>
+    <h2>SNACK 1</h2>
     <?php
     //Creato array contentente tutte le partite della giornata
     $arrPartite = [
@@ -90,7 +91,36 @@
     };
 
     ?>
+    <h2>SNACK 2</h2>
+    <form method="get">
+    <!-- Form per prendere i dati dell'utente -->
+        <div>
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome">
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email">
+        </div>
+        <div>
+            <label for="age">Età:</label>
+            <input type="number" id="age" name="età">
+        </div>
+        <div><input type="submit"></div>
+    </form>
+    <?php
+    if ((isset($_GET['nome']) && (isset($_GET['email'])) && (isset($_GET['età'])))) {
+        $nome = $_GET['nome'];
+        $email = $_GET['email'];
+        $età = $_GET['età'];
 
+        if (strlen($nome) > 3 && is_numeric($età) && strpos($email, "@") && strpos($email, ".")) {
+            echo "<div>Accesso consentito</div>";
+        } else {
+            echo "<div>Accesso negato</div>";
+        }
+    }
+    ?>
 </body>
 
 </html>
